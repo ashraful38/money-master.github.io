68b9a81cd4db6657058f87b439e757b2aa39e4a5
Ashraful Alam Limon <ashrafullimon38@gmail.com>
2023-01-30 18:30:14 +0600

    5th commit

--- js/calculate.js
+++ js/calculate.js
@@ -1,38 +1,97 @@
-function inputElementValueId(elementId) {
+function getInputElementValueId(elementId) {
     const elementField = document.getElementById(elementId);
     const inputValueString = elementField.value;
     const inputValue = parseFloat(inputValueString);
 
     return inputValue;
+   
+}
+
+function dd(elementId){
+    const elementField = document.getElementById(elementId);
+    //elementField.value = '';
+    return elementField.value;
+
+}
+
+
+
+function setTextElementValueById(elementId, newValue) {
+    const textElementId = document.getElementById(elementId);
+    textElementId.innerText = newValue;
 }
 
 
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const income = inputElementValueId('income');
+    const income = getInputElementValueId('income');
+
+
+    const food = getInputElementValueId('food');
+    const foodField = dd('food')
+    const rent = getInputElementValueId('rent');
+    const rentField = dd('rent');
+    const clothes = getInputElementValueId('clothes');
+    const clothesFiled = dd('clothes');
     
 
-    const food = inputElementValueId('food');
-    const rent = inputElementValueId('rent');
-    const clothes = inputElementValueId('clothes');
 
-    if (income < 0 && income == "") {
-        alert('Input must be positive Number');
-        console.log('input');
+
+
+
+    if (rentField === '' || foodField === '' || clothesFiled === '' ) {
         
+        alert('please give the number')
+    }
+    else if (income<0 || food < 0 || rent < 0 || clothes < 0) {
+        alert('Input must be positive Number');
     } 
-    else {
+    else{
         const expense = food + rent + clothes;
         const balance = income - expense;
         if (expense > income) {
             alert("Expenses cannot be more than income");
         } else {
             // view total expense and balance
-            document.getElementById("total-expense").innerText = expense;
-            document.getElementById("balance").innerText = balance;
+            setTextElementValueById('total-expense', expense);
+            //document.getElementById("total-expense").innerText = expense;
+            setTextElementValueById('balance', balance);
         }
+
     }
 
     
+
+
+
+
+
+
+
+})
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    const save = getInputElementValueId('save');
+    const income = getInputElementValueId('income');
+    //const balance = inputElementValueId('balance')
+    if (save < 0 || save > 100) {
+        alert("Provide positive and less than 100 saving value");
+        return;
+    }
+
+    savingAmount = (save / 100) * income;
+    const balance = document.getElementById("balance").innerText;
+    const remainingBalance = balance - savingAmount;
+
+    //   validate saving amount
+    if (savingAmount > balance) {
+        alert("SavingAmount is greater than balance");
+    } else {
+        // view saving amount and remaining balance
+        document.getElementById("saving-amount").innerText = savingAmount;
+        document.getElementById("remaining-balance").innerText = remainingBalance;
+    }
+
+
 
 })
